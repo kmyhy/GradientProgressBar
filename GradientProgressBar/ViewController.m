c25b11b982f26e005bb7f23ecb4d9f2377470f13
kmyhy <kmyhy@126.com>
2017-07-03 16:04:00 +0800

    Initial Commit

---
+++ GradientProgressBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GradientProgressBar
+//
+//  Created by qq on 2017/7/3.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
